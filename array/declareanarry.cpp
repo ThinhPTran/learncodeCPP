687e8b110df3f58a11bb7a4bd5d9e4a1f9347620
Thinh P. Tran <trphthinh@gmail.com>
2017-09-18 16:56:30 +0700

    Use array in c++11.

---
+++ array/declareanarry.cpp
@@ -0,0 +1,10 @@
+#include <iostream> 
+#include <array> 
+
+using namespace std; 
+
+int main() {
+  array<int, 3> myarray; // declare an integer array with length 3 
+}
+
+
---
+++ array/useinitializerlistforarray.cpp
@@ -0,0 +1,10 @@
+#include <iostream> 
+#include <array> 
+
+using namespace std; 
+
+int main() {
+  std::array<int, 5> myarray = {9,7,5,3,1}; 
+}
+
+
